c6fb92e24c9a62e2d1498ec4c54f06448d2d2b58
Xantibody <zeku.bushinryu38@gmail.com>
2025-04-23 01:10:58 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in { 
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.hello ];
+      };
+      packages.${system}.default = pkgs.hello;
+    };
+}
